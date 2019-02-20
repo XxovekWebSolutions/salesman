fa1df53057f73e767914ce0add769d134bd7ca56
vikas pawar <vikaspawar3110@gmail.com>
2019-02-20 12:30:41 +0530

    spaces removed

--- config/connection.php
+++ config/connection.php
@@ -3,7 +3,5 @@ $servername = 'localhost';
 $username   = 'root';
 $password   = '';
 $dbname     = 'Sales1';
-
 $con = new mysqli($servername,$username,$password,$dbname) or die(mysqli_error());
-
 ?>
